192faa18beca1aaeaa3f1be6ed3d08ff1beb666a
Andrea Alarcón <andreaalarcon5299@gmail.com>
2022-10-26 10:41:44 +0200

    falta revisar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import contactsJson from "./contacts.json"
 
 function App() {
+  const [contacts, setContacts] = useState(contactsJson.slice(0, 5))
+  
+  function randomContact() {
+    const newArray = contacts.map(contact => {
+      return contact
+    })
+  
+    let randomNum = Math.floor(Math.random() * contactsJson.length)
+  
+    newArray.push(contactsJson[randomNum])
+    contactsJson.splice(randomNum, 1)
+    setContacts(newArray)
+  }
+
+  function sortByName() {
+    const copyContact = [...contacts]
+    const sortName = copyContact.sort((a, b) => a.name > b.name ? 1 : -1)
+    setContacts(sortName)
+  }
+
+  function sortByPopularity() {
+    const copyContact = [...contacts]
+    const sortPopularity = copyContact.sort((a, b) => b.popularity - a.popularity)
+    setContacts(sortPopularity)
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App' >
+      <h1>IronContacts</h1>
+      <button onClick={randomContact}>Add Random Contact</button>
+      <button onClick={sortByName}>Sort by name</button>
+      <button onClick={sortByPopularity}>Sort by popularity</button>
+    
+    <table>
+      <thead>
+        <tr>
+          <th>Picture</th>
+          <th>Name</th>
+            <th>Popularity</th>
+            <th>Won Oscar</th>
+          <th>Won Emmy</th>     
+        </tr>
+      </thead>
+      <tbody>
+          {contacts.map(contact => {
+            return (
+              <tr>
+                <td>
+                  <img  src={contact.pictureUrl} alt='contact' />
+                </td>
+                <td>{contact.name}</td>
+                <td>{contact.popularity.toFixed(2)}</td>
+                {contact.wonOscar && <td>🏆</td>}
+                {contact.wonEmmy && <td>🏆</td>}
+              </tr>
+            )
+          })}
+      </tbody>
+    </table>
+     
     </div>
   );
 }
